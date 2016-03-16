cf0384b19bb0f657b20d697ada26e51cd8b529a1
shkang <shkang@jangin.com>
2016-03-16 16:52:16 +0900

    test9

--- widgets/login_info/skins/xe_official/js/login.js
+++ widgets/login_info/skins/xe_official/js/login.js
@@ -23,39 +23,3 @@ jQuery(function($){
 		} catch(e){};
 	}
 });
-
-
-
-/* Google SSO */
-var googleUser = {};
-var startApp = function() {
-	gapi.load('auth2', function() {
-						auth2 = gapi.auth2.init({
-							client_id : '901872542156-l43rnlajp3tgup8lloagvof2de46djcf.apps.googleusercontent.com',
-									cookiepolicy : 'single_host_origin',
-									scope: 'profile email'
-								});
-						attachSignin(document.getElementById('customBtn'));
-					});
-};
-
-function attachSignin(element) {
-	auth2.attachClickHandler(element, {}, function(googleUser) {
-		var id_token = googleUser.getAuthResponse().id_token;
-		console.log("ID Token: " + id_token);
-
-		var xhr = new XMLHttpRequest();
-		xhr.open('POST', '/login');
-		xhr.setRequestHeader('Content-Type',
-				'application/x-www-form-urlencoded');
-		xhr.send('idtoken=' + id_token);
-		xhr.onload = function() {
-			console.log('Signed in as: ' + xhr.responseText);
-			//location.href = '/index';
-		};
-		
-	}, function(error) {
-		alert('error!');
-		console.log(JSON.stringify(error, undefined, 2));
-	});
-}
