f569ee643b11935496a31e7311d7fc8d20f8dea7
shkang <shkang@jangin.com>
2016-03-17 18:48:02 +0900

    test40

--- modules/socialxe/socialxe.controller.php
+++ modules/socialxe/socialxe.controller.php
@@ -24,8 +24,9 @@
 			if(!$email_address) return new Object(-1, "msg_invalid_request");
 			
 			// @jangin.com 검사
-			if(strpos($email_address, '@jangin.com') === false)
-				return new Object(-1, "msg_invalid_request");
+			if(strpos($email_address, '@jangin.com') === false){
+				$error = 'msg_exists_email_address';
+			}
 			
 			$oMemberModel = getModel('member');
 			$member_srl = $oMemberModel->getMemberSrlByEmailAddress($email_address);
